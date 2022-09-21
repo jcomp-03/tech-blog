b54a94b7315d5db03d768277106c4433da9be033
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-09-21 14:17:43 -0400

    Modified server.js to work with handlebars.js. Added main and homepage to view folder. Added home-routes.js to controllers folder.

--- controllers/api/blog-post-routes.js
+++ controllers/api/blog-post-routes.js
@@ -1,133 +1,133 @@
-const router = require('express').Router();
-const { User, Blog_Post, Comment } = require('../../models');
+const router = require("express").Router();
+const { User, Blog_Post, Comment } = require("../../models");
 
 // HTTP GET request ALL blog posts JOIN user information
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // notice that when we return all the blog posts, we return
   // the username who made the blog post, but also the username of
   // whoever left a comment (that's the purpose of nested include)
   Blog_Post.findAll({
-    attributes: ['id', 'title', 'content', 'created_at'],
-    order: [['created_at', 'DESC']],
+    attributes: ["id", "title", "content", "created_at"],
+    order: [["created_at", "DESC"]],
     include: [
       // First LEFT OUTER JOIN blog post with user
       {
         model: User,
-        attributes: ['username']
+        attributes: ["username"],
       },
       // Second LEFT OUTER JOIN blog post with comment
-      {  
+      {
         model: Comment,
-        attributes: ['id', 'comment_text', 'created_At'],
+        attributes: ["id", "comment_text", "created_At"],
         include: [
           // Third LEFT OUTER JOIN comment with user
           {
             model: User,
-            attributes: ['username']
-          }
-        ]
-      }
-    ]
+            attributes: ["username"],
+          },
+        ],
+      },
+    ],
   })
-    .then(dbBlogPostData => res.json(dbBlogPostData))
-    .catch(err => {
+    .then((dbBlogPostData) => res.json(dbBlogPostData))
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
 // HTTP GET request SINGLE blog post JOIN user information
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   Blog_Post.findOne({
     where: {
-      id: req.params.id
+      id: req.params.id,
     },
-    attributes: ['id', 'title', 'content', 'created_at'],
+    attributes: ["id", "title", "content", "created_at"],
     include: [
       {
         model: User,
-        attributes: ['username']
+        attributes: ["username"],
       },
       {
         model: Comment,
-        attributes: ['id', 'comment_text', 'created_At'],
+        attributes: ["id", "comment_text", "created_At"],
         include: [
           {
             model: User,
-            attributes: ['username']
-          }
-        ]
-      }
-    ]
+            attributes: ["username"],
+          },
+        ],
+      },
+    ],
   })
-    .then(dbBlogPostData => {
+    .then((dbBlogPostData) => {
       if (!dbBlogPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
+        res.status(404).json({ message: "No post found with this id" });
         return;
       }
       res.json(dbBlogPostData);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
 // HTTP POST request SINGLE blog post
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Blog_Post.create({
     title: req.body.title,
     content: req.body.content,
-    user_id: req.body.user_id
+    user_id: req.body.user_id,
   })
-    .then(dbBlogPostData => res.json(dbBlogPostData))
-    .catch(err => {
+    .then((dbBlogPostData) => res.json(dbBlogPostData))
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
 // HTTP PUT request SINGLE blog post update title
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   Blog_Post.update(
     {
-      title: req.body.title
+      title: req.body.title,
     },
     {
       where: {
-        id: req.params.id
-      }
+        id: req.params.id,
+      },
     }
   )
-    .then(dbBlogPostData => {
+    .then((dbBlogPostData) => {
       if (!dbBlogPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
+        res.status(404).json({ message: "No post found with this id" });
         return;
       }
       res.json(dbBlogPostData);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
 // HTTP DELETE request SINGLE blog post
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   Blog_Post.destroy({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-    .then(dbBlogPostData => {
+    .then((dbBlogPostData) => {
       if (!dbBlogPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
+        res.status(404).json({ message: "No post found with this id" });
         return;
       }
       res.json(dbBlogPostData);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
---
+++ controllers/home-routes.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const sequelize = require('../config/connection');
+const { Blog_Post, User, Comment } = require('../models');
+
+router.get("/", (req, res) => {
+    Blog_Post.findAll({
+      attributes: ["id", "title", "content", "created_at"],
+      order: [["created_at", "DESC"]],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          attributes: ["id", "comment_text", "created_At"],
+          include: [
+            {
+              model: User,
+              attributes: ["username"],
+            }
+          ]
+        }
+      ]
+    })
+      .then((dbBlogPostData) => {
+        console.log('dbBlogPostData is', dbBlogPostData[0].get({ plain: true }));
+        // map through blog post data and serialize each blog post object
+        // the map will return an array which we capture in blogPosts
+        const blogPosts = dbBlogPostData.map( blog => blog.get({ plain: true}));
+        // evn though blogPosts is an array, pass it in as an object
+        // property to retain the ability to add/modify object properties
+        res.render('homepage', { blogPosts});
+    })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
