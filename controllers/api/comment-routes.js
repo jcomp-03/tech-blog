baeb4f6e25a8624bb7913200020ae09ffbe9472e
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-09-20 22:56:38 -0400

    Developed comment routes. Tested comment routes in Insomnia.

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+// HTTP GET request ALL comments
+router.get('/', (req, res) => {
+  Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// HTTP POST request SINGLE comment
+router.post('/', (req, res) => {
+  // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.body.user_id,
+    post_id: req.body.post_id
+  })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+// HTTP DELETE request SINGLE blog post
+router.delete('/:id', (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCommentData => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'No comment found with this id!' });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
